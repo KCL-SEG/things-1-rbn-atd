6d17afc7d9a46d4e2fb5c0184829fc1ebbf38d81
Reuben Atendido <K21048267@kcl.ac.uk>
2022-10-08 22:56:42 +0100

    remove blank flag on description

--- things/models.py
+++ things/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from django.db.models import Model
 from django.core.validators import MaxValueValidator, MinValueValidator
@@ -5,6 +6,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(Model):
     name = models.CharField(max_length = 30, unique = True, blank = False)
 
-    description = models.CharField(max_length = 120, blank = False)
+    description = models.CharField(max_length = 120, blank = False, unique=False)
 
-    quantity = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], blank = False)
+    quantity = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], unique = False)
