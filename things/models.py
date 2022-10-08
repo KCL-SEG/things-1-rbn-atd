9e0668baa2e493ca406c7562af45b60e624802da
Reuben Atendido <K21048267@kcl.ac.uk>
2022-10-08 22:04:08 +0100

    refine thing model

--- things/models.py
+++ things/models.py
@@ -1,8 +1,10 @@
+from enum import unique
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Things(models.Model):
-    name = models.CharField()
+class Thing(models.Model):
+    name = models.CharField(max_length = 30, unique = True, blank = False)
 
-    description = models.TextField()
+    description = models.CharField(max_length = 120, blank = False)
 
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(MaxValueValidator(100), MinValueValidator(0), blank = False)
